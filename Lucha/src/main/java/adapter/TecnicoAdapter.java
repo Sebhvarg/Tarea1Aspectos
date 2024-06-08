45fc5334e801ad8eaac8e05fe6290189531d73c7
CrisPih <152130690+CrisPih@users.noreply.github.com>
2024-06-07 22:35:41 -0500

    Update TecnicoAdapter.java

--- Lucha/src/main/java/adapter/TecnicoAdapter.java
+++ Lucha/src/main/java/adapter/TecnicoAdapter.java
@@ -3,32 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package adapter;
-import tipoLuchadores.Luchador;
-import tipoLuchadores.Tecnico;
-import interfaces.AtaqueNormal;
+
 /**
  *
  * @author sebas
  */
-public class TecnicoAdapter implements AtaqueNormal{
-
-    private Tecnico tecnico;
-
-    public TecnicoAdapter(Tecnico tecnico) {
-        this.tecnico = tecnico;
-        
-       
-    }
-
-    @Override
-    public double cambioDePuntos() {
-        return tecnico.llaveSubmission();
-    }
-    
-
-
- 
-    
-
-    
+public class TecnicoAdapter{   
+   
 }
